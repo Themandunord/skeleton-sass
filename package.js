5d298a05b9923ec3bb160d9a8084fe4c7afd845d
Lespagnol Rémy <remy.lespagnol@gmail.com>
2016-11-12 13:15:08 +0100

    Publish

--- package.js
+++ package.js
@@ -8,51 +8,83 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-    api.use('fourseven:scss@3.10.0');
+    api.versionsFrom('1.2.1');
+    api.use("fourseven:scss@3.4.1", ["client"]);
+    api.imply("fourseven:scss", ["client"]);
     api.add_files([
         'Skeleton-Sass/scss/base/_base-styles.scss'
+
+
 
         
         , 'Skeleton-Sass/scss/base/_functions.scss'
 
+
+
         
         , 'Skeleton-Sass/scss/base/_normalize.scss'
+
+
 
         
         , 'Skeleton-Sass/scss/base/_typography.scss'
 
+
+
         
         , 'Skeleton-Sass/scss/base/_utils.scss'
+
+
 
         
         , 'Skeleton-Sass/scss/base/_variables.scss'
 
+
+
         
         , 'Skeleton-Sass/scss/modules/_buttons.scss'
+
+
 
         
         , 'Skeleton-Sass/scss/modules/_code.scss'
 
+
+
         
         , 'Skeleton-Sass/scss/modules/_forms.scss'
+
+
 
         
         , 'Skeleton-Sass/scss/modules/_grid.scss'
 
+
+
         
         , 'Skeleton-Sass/scss/modules/_lists.scss'
+
+
 
         
         , 'Skeleton-Sass/scss/modules/_media-queries.scss'
 
+
+
         
         , 'Skeleton-Sass/scss/modules/_spacing.scss'
+
+
 
         
         , 'Skeleton-Sass/scss/modules/_tables.scss'
 
+
+
         
         , 'Skeleton-Sass/scss/skeleton.scss'
+
+
 
         
         , ], ['client']);
